a1a086561cde5723b2bcb1657ce38532ebb40b78
vito115 <vitodimaga@gmail.com>
2026-03-02 17:24:02 +0500

    Initial commit

---
+++ src/main/java/com/star/recommender/StarBankRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.star.recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankRecommenderApplication.class, args);
+	}
+
+}
